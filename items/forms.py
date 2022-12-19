543052d576fe63b9677b342c973965b8377d16bf
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-12-19 23:02:08 +0300

    fixed items forms

--- items/forms.py
+++ items/forms.py
@@ -10,7 +10,7 @@ class CategoryForm(forms.ModelForm):
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ["category", "title", "image"]
+        fields = ["category", "title", "image","description","price","image"]
 
 
 
