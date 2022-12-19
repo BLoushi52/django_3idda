ffb00f681dce3317ab088a27763ed17ddb39c27c
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-12-19 20:26:16 +0300

    fixed models

--- items/admin.py
+++ items/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Category, Item
+from .models import Category, Item, Order, Review, Favorite
 
-admin.site.register([Category, Item])
+admin.site.register([Category, Item, Order, Review, Favorite])
