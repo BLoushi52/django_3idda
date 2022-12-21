109e57cffa63db5fc4d7e3f08bf28de570f963d8
Khaled ALBuloushi <k.albloushii@hotmail.com>
2022-12-21 18:55:13 +0300

    made some changes on models, and updated the way that models look in admin panel

--- items/models.py
+++ items/models.py
@@ -9,34 +9,37 @@ class Category(models.Model):
     title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="media/")
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return f"{self.title}"
 
 class Item(models.Model):
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="itemes"
+        Category, on_delete=models.CASCADE, related_name="items"
     )
     title = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
+    description = models.TextField(max_length=500)
     price = models.FloatField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField()
+
     
 
     def __str__(self):
-        return f"{self.id}: {self.user.username} : {self.title}, {self.category.title}"
+        return f"{self.title}"
         
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(
-        Item, on_delete=models.CASCADE, related_name="orderes"
+        Item, on_delete=models.CASCADE, related_name="orders"
     )
     price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=100)
-    order_duration = models.FloatField()
+    order_duration = models.PositiveIntegerField()
     address = models.ForeignKey(Address, on_delete=models.PROTECT)
 
 class Review(models.Model):
