f5cab7974f220dca28efec89d09c49fb06509af5
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-12-22 09:30:05 +0300

    done >> OrderCreateView

--- django_3idda/urls.py
+++ django_3idda/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from items.views import CategoryView, ItemCreateView, ItemDeleteView, ItemUpdateView, ItemView, MyItemView, create_category, create_item, get_items, home
+from items.views import CategoryView, ItemCreateView, ItemDeleteView, ItemUpdateView, ItemView, MyItemView, OrderCreateView, create_category, create_item, get_items, home
 from accounts.views import AddressUpdateView, ChangePasswordView, UserCreateAPIView, UserLoginAPIView, MyAddressView, AddressCreateView
 from accounts.views import user_register, logout_user, login_user, edit_profile
 
@@ -37,6 +37,7 @@ urlpatterns = [
     path('api/addresses/', MyAddressView.as_view(), name='my_addresses_list-api'),
     path('api/address/create/', AddressCreateView.as_view(), name='create-address-api'),
     path('api/address/edit/<int:address_id>/', AddressUpdateView.as_view(), name='edit-address-api'),
+    path('api/order/create/', OrderCreateView.as_view(), name='create-order-api'),
 
 ]
 
--- items/serializers.py
+++ items/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from items.models import Category, Item
+from items.models import Category, Item, Order
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -23,3 +23,11 @@ class ItemUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['title','image', 'category','description','price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
+    status = serializers.CharField(read_only=True)
+    class Meta:
+        model = Order
+        fields = ['id', "user", "item", 'order_duration', 'address','price', 'status']
