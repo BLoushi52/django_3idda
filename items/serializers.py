8e6ad8f69c470b7fa68a7a886fe22f4401c10957
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-12-26 19:30:03 +0300

    fixed some issues with item and favorite

--- items/serializers.py
+++ items/serializers.py
@@ -14,6 +14,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class ItemSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     id = serializers.IntegerField(read_only=True)
+    category = serializers.CharField(source = 'category.title', read_only=True)
     class Meta:
         model = Item
         fields = ['id', 'title', "user", "category",'image', 'price', 'description']
@@ -23,6 +24,7 @@ class OrderSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     id = serializers.IntegerField(read_only=True)
     status = serializers.CharField(read_only=True)
+    item = ItemSerializer()
     class Meta:
         model = Order
         fields = ['id', "user", "item", 'order_duration', 'address','price', 'status']
